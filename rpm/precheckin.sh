062cdcda7086086d644042dbc214e03ade26b689
Matti Lehtimäki <matti.lehtimaki@jolla.com>
2024-06-06 17:26:22 +0300

    [gcc] Update to version 13.3.1. JB#62204
    Switch to using submodule.

---
+++ rpm/precheckin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAME=gcc
+SPECNAME=${NAME}.spec
+ARCHES="armv7hl i486 x86_64 aarch64"
+# If your %_vendor changes, please edit this too --cvm
+VENDOR=meego
+TOBASELIBS=""
+TOBASELIBS_ARCH=""
+
+for i in ${ARCHES} ; do
+# cross spec files
+    cat ./${SPECNAME} | sed -e "s#Name: .*#Name: cross-${i}-${NAME}\n%define crossarch ${i}#" > ./cross-${i}-${NAME}.spec
+done
